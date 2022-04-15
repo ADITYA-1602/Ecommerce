9159e2b6a2cf5843ddccc850da00637b105c2711
ADITYA-1602 <raj.23@iitj.ac.in>
2022-04-15 14:44:57 +0530

    first commit

---
+++ frontend/build/precache-manifest.3fe9649f7650c46091fedf61f070df88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "665a813e4b401a168198",
+    "url": "/static/css/main.903fffd1.chunk.css"
+  },
+  {
+    "revision": "665a813e4b401a168198",
+    "url": "/static/js/main.665a813e.chunk.js"
+  },
+  {
+    "revision": "11cf982c8343d8397216",
+    "url": "/static/css/1.1f38e1e6.chunk.css"
+  },
+  {
+    "revision": "11cf982c8343d8397216",
+    "url": "/static/js/1.11cf982c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3101c01df449722d9d093865f9b442ef",
+    "url": "/static/media/playstore.3101c01d.png"
+  },
+  {
+    "revision": "d0891202e1665975b793eabf8ac28aaa",
+    "url": "/static/media/Appstore.d0891202.png"
+  },
+  {
+    "revision": "728216be61204d8fa61b63c9d9d0e1c1",
+    "url": "/static/media/cover.728216be.jfif"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/Profile.782adc2b.png"
+  },
+  {
+    "revision": "b381a18d1513aaa643ad7706ccc7b4ff",
+    "url": "/index.html"
+  }
+];
